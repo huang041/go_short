556484bd088cd25194089169f78316fd120e52a4
siying <siying_huang@moneynet.tw>
2025-04-02 23:27:35 +0800

    fix: Alow all ip

--- main.go
+++ main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"go_short/routers"
 	"go_short/models"
+	router "go_short/routers"
 	"log"
 )
-
-
 
 func main() {
 	models.InitDatabase()
 	log.Println(models.DB)
 	router := router.InitRouter()
-	router.Run("127.0.0.1:8080")
+	router.Run("0.0.0.0:8080")
 }
