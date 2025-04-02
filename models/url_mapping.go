d93c6ab2d4ac3c1027261c865423cb2b8b7d3244
siying <siying_huang@moneynet.tw>
2025-04-02 23:29:06 +0800

    fix: format

--- models/url_mapping.go
+++ models/url_mapping.go
@@ -1,11 +1,11 @@
 package models
 
 import (
-  "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 type UrlMapping struct {
 	gorm.Model `json:"id"`
 	Rename_url *string `json:"rename_url" gorm:"type:varchar(255);unique"`
-	Origin_url string `json:"origin_url" gorm:"type:varchar(255);unique"`
+	Origin_url string  `json:"origin_url" gorm:"type:varchar(255);unique"`
 }
