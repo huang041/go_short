ccdb10022f5d391c50a248b73b0b5560cbab1d55
siying <siying_huang@moneynet.tw>
2025-04-02 23:46:45 +0800

    fix: get url by json

--- controllers/url_mapping_controller.go
+++ controllers/url_mapping_controller.go
@@ -1,49 +1,60 @@
 package controllers
 
 import (
-	"github.com/gin-gonic/gin"
-	"net/http"
 	"go_short/models"
 	"go_short/services"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 type UrlMappingController struct{}
 
-func ( umc * UrlMappingController ) GetUrlMapping(c *gin.Context) {
+func (umc *UrlMappingController) GetUrlMapping(c *gin.Context) {
 	var urlMappings []models.UrlMapping
-    if err := models.DB.Find(&urlMappings).Error; err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{
-            "code": http.StatusInternalServerError,
-            "msg":  "Error while fetching data",
-        })
-        return
-    }
+	if err := models.DB.Find(&urlMappings).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code": http.StatusInternalServerError,
+			"msg":  "Error while fetching data",
+		})
+		return
+	}
 
-    c.JSON(http.StatusOK, gin.H{
-        "code": 200,
-        "msg":  "success",
-        "data": urlMappings,
-    })
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "success",
+		"data": urlMappings,
+	})
 }
 
-func ( umc * UrlMappingController ) SaveUrlMapping(c *gin.Context) {
-	originURL := c.Query("url")
-	urlMapping := models.UrlMapping{Rename_url: nil, Origin_url: originURL}
+func (umc *UrlMappingController) SaveUrlMapping(c *gin.Context) {
+	// Create a struct to bind the request body
+	var request struct {
+		URL string `json:"url" binding:"required"`
+	}
+
+	// Bind the request body to the struct
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request format or missing URL"})
+		return
+	}
+
+	urlMapping := models.UrlMapping{Rename_url: nil, Origin_url: request.URL}
 
 	if err := models.DB.Create(&urlMapping).Error; err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
-    }
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
 	id := int(urlMapping.ID)
 	urlMapping.Rename_url = services.DecimalToBase62(id)
 
 	if err := models.DB.Save(&urlMapping).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-        return
+		return
 	}
 	log.Println(&urlMapping)
 	c.JSON(200, gin.H{
-			"short_url": urlMapping.Rename_url,
-		})
+		"short_url": urlMapping.Rename_url,
+	})
 }
