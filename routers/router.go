6cfb8c58c68643a6efe6d8ed90d4c7092c09c0a3
siying <siying_huang@moneynet.tw>
2025-04-03 16:15:20 +0800

    rename router

--- routers/router.go
+++ routers/router.go
@@ -1,20 +1,21 @@
-package router
+package routers
 
-import(
-	"github.com/gin-gonic/gin"
+import (
 	. "go_short/controllers"
+
+	"github.com/gin-gonic/gin"
 )
 
-func InitRouter() *gin.Engine{
+func InitRouter() *gin.Engine {
 	router := gin.Default()
 	router.GET("/ping", Ping)
 
 	urlMappingController := UrlMappingController{}
 	router.GET("/url_mapping", urlMappingController.GetUrlMapping)
 	router.POST("/url_mapping", urlMappingController.SaveUrlMapping)
-	
+
 	// Route for redirecting to original URL when visiting /{short_url}
 	router.GET("/:shortURL", urlMappingController.RedirectToOriginalUrl)
-	
+
 	return router
 }
